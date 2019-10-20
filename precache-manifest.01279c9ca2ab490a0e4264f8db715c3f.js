34c94823ec0611262d82f50ba65fa82a150965bb
Mina Yousseif <mina.yousseif@gmail.com>
2019-10-19 21:40:37 -0400

    Updates

---
+++ precache-manifest.01279c9ca2ab490a0e4264f8db715c3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53d78ee1e5d712eb8205deb8f7d99ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c74bac73dc3164b0bdbb",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c9152bb794716fe468be",
+    "url": "/static/css/main.0de2d219.chunk.css"
+  },
+  {
+    "revision": "c74bac73dc3164b0bdbb",
+    "url": "/static/js/2.13abe044.chunk.js"
+  },
+  {
+    "revision": "c9152bb794716fe468be",
+    "url": "/static/js/main.b075d437.chunk.js"
+  },
+  {
+    "revision": "f6576c48f2faaa6ceac9",
+    "url": "/static/js/runtime-main.be23565a.js"
+  },
+  {
+    "revision": "3f9772ae90aa17ec5cf5a72342310008",
+    "url": "/static/media/header-bg.3f9772ae.jpg"
+  },
+  {
+    "revision": "0f7554d942178cf2c3583ef9ef2dd8fb",
+    "url": "/static/media/my.0f7554d9.jpg"
+  }
+]);
